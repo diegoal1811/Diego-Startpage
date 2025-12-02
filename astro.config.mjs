8d756b8c77fd433295001d98550a392722291709
Diego Liremberg <diego-18-11@hotmail.com>
2025-12-01 20:36:57 -0600

    For Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://diegoal1811.github.io',
+  base: '/Diego-Startpage',
   vite: {
     plugins: [tailwindcss()]
   }
